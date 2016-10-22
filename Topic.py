e348eb897b0373215adf0246fd901e9c86c6b282
breez7 <james777.lee@gmail.com>
2016-10-23 01:00:46 +0900

    Android와 연동중 일부 수정된것

--- Topic.py
+++ Topic.py
@@ -2,33 +2,36 @@ import sys
 
 
 class Topic():
-	def __init__(self):
-		self.id = -1
-		self.title = ''
-		self.genere = ''
-		self.mediaFilePath = ''
-		self.script = ''
-		self.sttResult = None
+    def __init__(self):
+        self.id = -1
+        self.title = ''
+        self.genere = ''
+        self.mediaFilePath = ''
+        self.script = ''
+        self.sttResult = None
 
-	def createTopic(self, title, genere, mediaFilePath, script):
-		self.id = 1
-		self.title = title
-		self.genere = genere
-		self.mediaFilePath = mediaFilePath
-		self.script= script
-		return self.id
-	def updateTopic(self):
-		pass
-	def deleteTopic(self, id):
-		pass
-	def retriveTopic(self, id):
-		self.id = id
-		self.loadDBValue(id)
-		return self.title, self.genere, self.mediaFilePath, self.script, self.sttResult
+    def createTopic(self, title, genere, mediaFilePath, script):
+        self.id = 1
+        self.title = title
+        self.genere = genere
+        self.mediaFilePath = mediaFilePath
+        self.script = script
+        return self.id
 
-	def loadDBValue(self,id):
-		self.title = 'BBC English : ' + id
-		self.genere = 'News'
-		self.mediaFilePath = './UnitTest/bbc.mp3'
-		self.script = "words in the news from BBC learning English dot com it's been a history in the making the people of Scotland have decided to continue their three hundred GA union with England so the U. K. survives pro independence campaign to say that disappointed but insist the high turnout shows there's an appetite for change few would disagree and accept the results doesn't mean Britain goes back to business as usual in the hours and days ahead the prime minister David Cameron and the other party leaders will now have to deliver on that promise in the last days of the campaign to give Scotland more powers and no one believes that can be done without a wide a shakeup of how the rest of the UK is governed"
-		self.sttResult = None
+    def updateTopic(self):
+        pass
+
+    def deleteTopic(self, id):
+        pass
+
+    def retriveTopic(self, id):
+        self.id = id
+        self.loadDBValue(id)
+        return self.title, self.genere, self.mediaFilePath, self.script, self.sttResult
+
+    def loadDBValue(self, id):
+        self.title = 'BBC English'
+        self.genere = 'News'
+        self.mediaFilePath = './UnitTest/bbc.mp3'
+        self.script = "words in the news from BBC learning English dot com it's been a history in the making the people of Scotland have decided to continue their three hundred GA union with England so the U. K. survives pro independence campaign to say that disappointed but insist the high turnout shows there's an appetite for change few would disagree and accept the results doesn't mean Britain goes back to business as usual in the hours and days ahead the prime minister David Cameron and the other party leaders will now have to deliver on that promise in the last days of the campaign to give Scotland more powers and no one believes that can be done without a wide a shakeup of how the rest of the UK is governed"
+        self.sttResult = None
--- TopicManager.py
+++ TopicManager.py
@@ -4,53 +4,69 @@ from pydub import AudioSegment
 
 import json
 
-class TopicManager:
-	def __init__(self):
-		self.stt = IBMSTT()
 
-	def addTopic(self,title, genere, path, script):
-		topic = Topic()
-		topic.createTopic(title,genere,path,script)
-		return topic
-	def getTopic(self, id):
-		topic = Topic()
-		topic.retriveTopic(id)
-		return topic
-	def analyseTopic(self, topic):
-		sttResult = self.stt.translate(topic.mediaFilePath)
-		topic.sttResult = sttResult
-		topic.updateTopic()
-		return topic
-	def getAllTopics(self):
-		return None
+class TopicManager:
+    def __init__(self):
+        self.stt = IBMSTT()
+
+    def addTopic(self, title, genere, path, script):
+        topic = Topic()
+        topic.createTopic(title, genere, path, script)
+        return topic
+
+    def getTopic(self, id):
+        topic = Topic()
+        topic.retriveTopic(id)
+        return topic
+
+    def analyseTopic(self, topic):
+        sttResult = self.stt.translate(topic.mediaFilePath)
+        topic.sttResult = sttResult
+        topic.updateTopic()
+        return topic
+
+    def getAllTopics(self):
+        # TODO
+        topica = Topic()
+        topica.id = 1
+        topica.title = 'BBC English'
+        topicb = Topic()
+        topicb.id = 2
+        topicb.title = 'CNN English'
+        topics = [topica, topicb]
+        return topics
+
 
 class IBMSTT:
-	URL = 'https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true&continuous=true'	
-	USERNAME = "12dd2d69-9bb7-45c7-8bc9-c193bd2dba63"
-	PASSWORD = "rnAnWqvZvHzt"
-	HEADERS = {"Content-Type":"audio/wav","X-Waston-Learning-Opt-Out":"true"}
-	def translate(self, filePath):
-		#data = open("woman1_wb.wav", "rb")
-		filePath = self.convertToWav(filePath)
-		data = open(filePath, "rb")
-		res = requests.post(IBMSTT.URL, data=data, auth=(IBMSTT.USERNAME, IBMSTT.PASSWORD), headers=IBMSTT.HEADERS)
-		data.close()
-		return self.parseSTTResult(res.text)
+    URL = 'https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true&continuous=true'
+    USERNAME = "12dd2d69-9bb7-45c7-8bc9-c193bd2dba63"
+    PASSWORD = "rnAnWqvZvHzt"
+    HEADERS = {"Content-Type": "audio/wav",
+               "X-Waston-Learning-Opt-Out": "true"}
 
-	def convertToWav(self,src):
-		if src.lower().endswith('mp3'):
+    def translate(self, filePath):
+        #data = open("woman1_wb.wav", "rb")
+        filePath = self.convertToWav(filePath)
+        data = open(filePath, "rb")
+        res = requests.post(IBMSTT.URL, data=data, auth=(
+            IBMSTT.USERNAME, IBMSTT.PASSWORD), headers=IBMSTT.HEADERS)
+        data.close()
+        return self.parseSTTResult(res.text)
 
-			sound = AudioSegment.from_mp3(src)
-			src = src[:-3] + 'wav'
-			sound.export(src, format="wav")
-		return src
+    def convertToWav(self, src):
+        if src.lower().endswith('mp3'):
 
-	def parseSTTResult(self, sttResult):
-		sttResult = json.loads(sttResult)
-		sentences = []
-		result = sttResult['results']
-		for sentence in result:
-			transcript = sentence['alternatives'][0]['transcript']
-			words = sentence['alternatives'][0]['timestamps']
-			sentences.append([transcript, words])
-		return sentences	
+            sound = AudioSegment.from_mp3(src)
+            src = src[:-3] + 'wav'
+            sound.export(src, format="wav")
+        return src
+
+    def parseSTTResult(self, sttResult):
+        sttResult = json.loads(sttResult)
+        sentences = []
+        result = sttResult['results']
+        for sentence in result:
+            transcript = sentence['alternatives'][0]['transcript']
+            words = sentence['alternatives'][0]['timestamps']
+            sentences.append([transcript, words])
+        return sentences
